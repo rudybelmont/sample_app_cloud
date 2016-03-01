5074eee4258bb7e2a016167b75854487cabb26e2
rudy <rudy@mitrais.com>
2016-03-01 06:41:12 +0000

    Verify lower email test case(solves exercise 6.5.1)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,11 +9,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-<<<<<<< HEAD
-    assert_select "title", "Home | #{@base_title}"
-=======
     assert_select "title", "#{@base_title}"
->>>>>>> master
   end
 
   test "should get help" do
@@ -26,15 +22,12 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select "title", "About | #{@base_title}"
-<<<<<<< HEAD
-=======
   end
   
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
->>>>>>> master
+    assert_select "title", "Contact | #{@base_title}"
   end
   
 end
