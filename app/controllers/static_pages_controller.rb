43a8d0825375856b9f09767ab6bd3dd74d92f8a0
rudy <rudy@mitrais.com>
2016-03-01 03:32:55 +0000

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,21 +1,13 @@
-require 'test_helper'
-
-class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
-    get :home
-    assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+class StaticPagesController < ApplicationController
+  def home
   end
 
-  test "should get help" do
-    get :help
-    assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  def help
   end
-
-  test "should get about" do
-    get :about
-    assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  
+  def about
+  end
+  
+  def contact
   end
 end
